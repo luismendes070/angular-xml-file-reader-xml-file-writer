32a51df1ad15d76f1c7a2d7117acc9d67c44c956
luismendes070 <machadoluismendes@gmail.com>
2023-03-13 15:12:11 -0300

    webpack config lodash config

---
+++ src/index.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:83a7a08f002cbe0ef81f16f633f95266768a7dff878b780875d35c6d33b2065b
+size 548
---
+++ webpack.config.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1ed88e2d55f05552cab6217f86061361e632f8fc47baa165359e3e9cbd4e69b3
+size 900
