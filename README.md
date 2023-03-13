adcc8262aca3fce83d8e0eee13d99c9be61632a5
luismendes070 <machadoluismendes@gmail.com>
2023-03-13 04:49:49 -0300

    3 files modified, 1 deleted

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # angular-xml-file-reader-xml-file-writer
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-kes4fw)
+
+
+https://therichpost.com/angular-15-read-xml-file-data-working-demo/
--- src/XmlFileReader.ts
+++ src/XmlFileReader.ts
@@ -3,10 +3,10 @@ import * as xml2js from 'xml2js';
 
 try{
 // Read the XML file
-const xml = fs.readFileSync('./assets/users.xml', 'utf-8');
+const xml:any = fs.readFileSync('./assets/users.xml', 'utf-8');
 
 // Convert the XML to a JavaScript object
-xml2js.parseString(xml, (err, result) => {
+xml2js.parseString(xml, (err:any, result:any) => {
   if (err) {
     console.error(err);
     return;
