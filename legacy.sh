0a3663b0c420ca2191db64fc5bbdeffe00cf0b2a
luismendes070 <machadoluismendes@gmail.com>
2023-03-13 09:48:24 -0300

    ng update

---
+++ legacy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+npm i --legacy-peer-deps --include=dev @angular/compiler-cli@"^15.1.0-next.2" @angular-devkit/build-angular@"15.1.0-next.2" @angular-devkit/build-angular@"15.1.0-next.2"
